f480f89250ab72f4721425cd623ca8984ea181bb
Afabus22 <126599292+Afabus22@users.noreply.github.com>
2023-04-11 20:05:45 +0300

    Проект

--- main.py
+++ main.py
@@ -1,80 +1,87 @@
-
 NAME1 = input("Ведите имя первого игрока\n")
 NAME2 = input("Введите имя второго игрока\n")
 # Инициализация карты
-maps = ['⬜','⬜','⬜',
-        '⬜','⬜','⬜',
-        '⬜','⬜','⬜']
- 
+maps = ['⬜', '⬜', '⬜',
+        '⬜', '⬜', '⬜',
+        '⬜', '⬜', '⬜']
+
 # Инициализация победных линий
-victories = [[0,1,2],
-             [3,4,5],
-             [6,7,8],
-             [0,3,6],
-             [1,4,7],
-             [2,5,8],
-             [0,4,8],
-             [2,4,6]]
- 
+victories = [[0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8],
+             [0, 3, 6],
+             [1, 4, 7],
+             [2, 5, 8],
+             [0, 4, 8],
+             [2, 4, 6]]
+
 # Вывод карты на экран
+
+
 def print_maps(maps):
-    print(maps[0], end=" ")
-    print(maps[1], end=" ")
-    print(maps[2])
- 
-    print(maps[3], end=" ")
-    print(maps[4], end=" ")
-    print(maps[5])
- 
-    print(maps[6], end=" ")
-    print(maps[7], end=" ")
-    print(maps[8])    
+    start = 0
+    end = 3
+    for row in range(1, 4):
+        print(*maps[start:end])
+        start += 3
+        end += 3
+    # print(maps[0], end=" ")1
+    # print(maps[1], end=" ")
+    # print(maps[2])
+
+    # print(maps[3], end=" ")
+    # print(maps[4], end=" ")
+    # print(maps[5])
+
+    # print(maps[6], end=" ")
+    # print(maps[7], end=" ")
+    # print(maps[8])
 
 
- 
 # Сделать ход в ячейку
 def step_maps(step, symbol):
-    ind = maps.index(step)
-    maps[ind] = symbol
- 
+    maps[step] = symbol
+
 # Получить текущий результат игры
+
+
 def get_result():
-    win = ""
- 
+    win = ''
     for i in victories:
         if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
             win = NAME1
         if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
-            win = NAME1
-             
+            win = NAME2
+
     return win
- 
+
+
 # Основная программа
 game_over = False
 player1 = True
- 
+
 while game_over == False:
- 
+
     # 1. Показываем карту
     print_maps(maps)
- 
+
     # 2. Спросим у играющего куда делать ход
     if player1 == True:
         symbol = "X"
-        step = int(input(f"{NAME1}, твой ход: "))
+        step = int(input(f"{NAME1}, твой ход: ")) - 1
     else:
         symbol = "O"
-        step = int(input(f"{NAME2}, твой ход: "))
- 
-    step_maps(step, symbol) # делаем ход в указанную ячейку
-    win = get_result() # определим победителя
+        step = int(input(f"{NAME2}, твой ход: ")) - 1
+
+    step_maps(step, symbol)  # делаем ход в указанную ячейку
+    win = get_result()  # определим победителя
     if win != "":
         game_over = True
     else:
         game_over = False
- 
-    player1 = not(player1)        
 
-# Игра окончена. Покажем карту. Объявим победителя.        
-print_maps()
+    player1 = not (player1)
+
+# Игра окончена. Покажем карту. Объявим победителя.
+print_maps(maps)
 print("Победил", win)
